21b61a0fffb9bba59ae1e5f97ca025bee0a2b89d
haris-begic <haris.begic@shopcircle.co>
2024-03-24 16:59:58 +0100

    Modified and cleaner code

--- multi-page form/script.js
+++ multi-page form/script.js
@@ -1,164 +1,189 @@
-const formContent = document.querySelectorAll('.form-content');
-const progressSteps = document.querySelectorAll('.step');
+document.addEventListener('DOMContentLoaded', function () {
+    // Main selectors in the document
+    const formContent = document.querySelectorAll('.form-content');
+    const progressSteps = document.querySelectorAll('.step');
+    const nextButtons = document.querySelectorAll('.next-button');
+    const backButtons = document.querySelectorAll('.back-button');
 
-const nextButtons = document.querySelectorAll('.next-button');
-const backButtons = document.querySelectorAll('.back-button');
+    let currentStep = 0; // Keep track of the current page
 
-const noteElements = document.querySelectorAll('.encouraging-note'); // Get all note elements
+    function nextPage() {
 
-const hiddenButton = document.querySelector('.hidden-button');
-const hiddenButton2 = document.querySelector('.hidden-button2');
+        // Hiding the current page and cycling through the next page by adding the active class
+        formContent[currentStep].classList.remove('active');
 
-const userInput = document.querySelector('.user-input');
-const fileInput = document.querySelector('.file-input');
-const formCheckbox = document.querySelector('.form-checkbox');
+        currentStep = (currentStep + 1) % formContent.length; // Cycle through pages
 
-const firstCheck = document.querySelector('#option1');
-const secondCheck = document.querySelector('#option2');
-const thirdCheck = document.querySelector('#option3');
-const fourthCheck = document.querySelector('#option4');
+        progressSteps[currentStep].classList.add('active');
+        formContent[currentStep].classList.add('active');
 
-userInput.addEventListener('input', validateAndShowNext);
-fileInput.addEventListener('change', validateAndShowNext); 
-formCheckbox.addEventListener('change', validateAndShowNext); 
+        console.log(currentStep);
+    }
 
-firstCheck.addEventListener('change', validateAndShowNext2);
-secondCheck.addEventListener('change', validateAndShowNext2);
-thirdCheck.addEventListener('change', validateAndShowNext2);
-fourthCheck.addEventListener('change', validateAndShowNext2);
+    function prevPage() {
+        formContent[currentStep].classList.remove('active');
+        progressSteps[currentStep].classList.remove('active');
 
-const radioGroups = document.querySelectorAll('.radio-group-1, .radio-group-2');
-radioGroups.forEach(group => {
-    const radioButtons = group.querySelectorAll('input[type="radio"]');
+        currentStep = (currentStep - 1 + formContent.length) % formContent.length; // Cycle through pages and make sure not to go into negative
 
-    radioButtons.forEach(button => {
-        button.addEventListener('change', validateAndShowNext3);
+        formContent[currentStep].classList.add('active');
+        progressSteps[currentStep].classList.add('active');
+
+        if (selectedImage) {
+            selectedImage.classList.remove('selected');
+            selectedImage = null;
+        }
+
+    }
+
+    function navigationButtons() {
+
+        nextButtons.forEach(button => {
+            button.addEventListener('click', nextPage);
+        });
+
+        backButtons.forEach(button => {
+            button.addEventListener('click', prevPage);
+        });
+    }
+
+    navigationButtons();
+
+    const cards = document.querySelectorAll('.card');
+
+    let selectedImage = null;
+
+    function handleTransition(event) {
+        if (selectedImage) {
+            selectedImage.classList.remove('selected');
+        }
+
+        event.target.classList.add('selected');
+        selectedImage = event.target;
+
+        setTimeout(nextPage, 300);
+    }
+
+    cards.forEach(card => {
+        card.addEventListener('click', handleTransition);
     });
+
+    // Function to initialize datepicker library 
+    function initializeDatepicker() {
+        // will populate
+    }
+
+    // Idea page (3)
+    const designIdeasSelect = document.getElementById('design-ideas');
+    const designIdeasOptions = document.getElementById('design-ideas-options');
+    const ideasOptions = document.getElementById('ideas-options');
+    const scratchOptions = document.getElementById('scratch-options');
+    const photosSelect = document.getElementById('photos');
+    const photosOptions = document.getElementById('photos-options');
+    const ideaButton = document.querySelector('.idea-button');
+    const ideaNote = document.querySelector('.idea-note');
+
+    function togglePageThree() {
+        if (designIdeasSelect.value && photosSelect.value) {
+            ideaButton.classList.remove('hidden');
+            ideaNote.classList.remove('hidden');
+        } else {
+            ideaButton.classList.add('hidden');
+            ideaNote.classList.add('hidden');
+        }
+    }
+
+    designIdeasSelect.addEventListener('change', function () {
+        if (this.value === 'ideas') {
+            designIdeasOptions.classList.remove('hidden');
+            ideasOptions.classList.remove('hidden');
+            scratchOptions.classList.add('hidden');
+        } else if (this.value === 'scratch') {
+            designIdeasOptions.classList.remove('hidden');
+            scratchOptions.classList.remove('hidden');
+            ideasOptions.classList.add('hidden');
+        } else {
+            designIdeasOptions.classList.add('hidden');
+            ideasOptions.classList.add('hidden');
+            scratchOptions.classList.add('hidden');
+        }
+    });
+
+    photosSelect.addEventListener('change', function () {
+        if (this.value === 'yes') {
+            photosOptions.classList.remove('hidden');
+        } else {
+            photosOptions.classList.add('hidden');
+        }
+    });
+
+    designIdeasSelect.addEventListener('change', function () {
+        togglePageThree();
+    });
+
+    photosSelect.addEventListener('change', function () {
+        togglePageThree();
+    });
+
+
+    // Timeline page (4)
+    const timelineButton = document.querySelector('.timeline-button');
+    const radioOptions = document.querySelectorAll('input[type="radio"][name="selection"]');
+    const timelineNote = document.querySelector('.timeline-note');
+
+    function togglePageFour() {
+        const anyOptionSelected = Array.from(radioOptions).some(option => option.checked);
+        timelineButton.style.display = anyOptionSelected ? 'block' : 'none';
+        timelineNote.style.display = anyOptionSelected ? 'block' : 'none';
+    }
+
+    radioOptions.forEach(option => {
+        option.addEventListener('change', togglePageFour);
+    });
+
+    // If the specific date option is selected, show the datepicker
+    const specificDateOption = document.getElementById('option4');
+    const datepickerContainer = document.getElementById('datepicker-container');
+    const datepicker = document.getElementById('datepicker');
+
+    specificDateOption.addEventListener('change', function () {
+        datepickerContainer.style.display = this.checked ? 'block' : 'none';
+    });
+    
+    // Function to toggle datepicker visibility
+    function toggleDatepickerVisibility() {
+        datepickerContainer.style.display = specificDateOption.checked ? 'block' : 'none';
+    }
+
+    // Event listener for specific date option
+    specificDateOption.addEventListener('change', toggleDatepickerVisibility);
+
+    // Event listeners for other radio options
+    radioOptions.forEach(option => {
+        if (option !== specificDateOption) { // Exclude specific date option
+            option.addEventListener('change', function() {
+                datepickerContainer.style.display = 'none'; // Hide datepicker
+            });
+        }
+    });
+
+    // Budget page (5)
+    const budgetButton = document.querySelector('.budget-button');
+    const minimumDropdown = document.getElementById('minimum');
+    const maximumDropdown = document.getElementById('maximum');
+    const budgetNote = document.querySelector('.budget-note');
+
+    function togglePageFive() {
+        const isMinimumSelected = minimumDropdown.value !== 'option1';
+        const isMaximumSelected = maximumDropdown.value !== 'option1';
+        const isValidSelection = isMinimumSelected && isMaximumSelected;
+
+        budgetButton.style.display = isValidSelection ? 'block' : 'none';
+        budgetNote.style.display = isValidSelection ? 'block' : 'none';
+    }
+
+    minimumDropdown.addEventListener('change', togglePageFive);
+    maximumDropdown.addEventListener('change', togglePageFive);
 });
 
-let currentStep = 0; // Keep track of the current page
-
-function nextPage() {
-
-    // Hiding the current page and cycling through the next page by adding the active class
-    formContent[currentStep].classList.remove('active');
-
-    currentStep = (currentStep + 1) % formContent.length; // Cycle through pages
-
-    formContent[currentStep].classList.add('active');
-    progressSteps[currentStep].classList.add('active');
-
-    if(currentStep == 2) {
-        validateAndShowNext();
-    }
-
-    if(currentStep == 4) {
-        validateAndShowNext2();
-    }
-
-    if(currentStep == 5) {
-        validateAndShowNext3();
-    }
-
-    console.log(currentStep);
-
-}
-
-function prevPage() {
-    formContent[currentStep].classList.remove('active');
-    progressSteps[currentStep].classList.remove('active');
-
-    currentStep = (currentStep - 1 + formContent.length) % formContent.length; // Cycle through pages and make sure not to go into negative
-
-    formContent[currentStep].classList.add('active');
-    progressSteps[currentStep].classList.add('active');
-
-    if (selectedImage) {
-        selectedImage.classList.remove('selected');
-        selectedImage = null;
-    }
-
-}
-
-function navigationButtons() {
-
-    nextButtons.forEach(button => {
-        button.addEventListener('click', nextPage);
-    });
-
-    backButtons.forEach(button => {
-        button.addEventListener('click', prevPage);
-    });
-}
-
-navigationButtons();
-
-const cards = document.querySelectorAll('.card');
-
-let selectedImage = null;
-
-function handleTransition(event) {
-    if (selectedImage) {
-        selectedImage.classList.remove('selected');
-    }
-
-    event.target.classList.add('selected');
-    selectedImage = event.target;
-
-    setTimeout(nextPage, 300); 
-}
-
-
-cards.forEach(card => {
-    card.addEventListener('click', handleTransition);
-});
-
-function validateAndShowNext() {
-    
-    // Check for input, file selection, and if the checkbox is checked
-    const isFormValid = userInput.value.length !== 0 || 
-                            fileInput.files.length > 0 || 
-                            formCheckbox.checked;
-
-    hiddenButton.style.display = isFormValid ? "block" : "none"; 
-    hiddenNote.style.display = isFormValid ? 'block' : 'none';
-}
-
-function validateAndShowNext2() {
-    
-    // Check for input, file selection, and if the checkbox is checked
-    const isCheckValid =  firstCheck.checked ||
-                        secondCheck.checked ||
-                         thirdCheck.checked ||
-                         fourthCheck.checked;
-
-    hiddenButton2.style.display = isCheckValid ? "block" : "none"; 
-    hiddenNote1.style.display = isCheckValid ? 'block' : 'none';
-}
-
-// Function to initialize datepicker library 
-function initializeDatepicker() {
-  // will populate
-}
-
-const dropdown1 = document.getElementById('dropdown1');
-const dropdown2 = document.getElementById('dropdown2');
-const hiddenButton3 = document.querySelector('.hidden-button3');
-const hiddenNote = document.querySelector('.note');
-const hiddenNote1 = document.querySelector('.note1');
-const hiddenNote2 = document.querySelector('.note2');
-
-function validateAndShowNext3() {
-  const isDropdown1Selected = dropdown1.selectedIndex !== 0; // Check if a non-default option is selected
-  const isDropdown2Selected = dropdown2.selectedIndex !== 0;
-
-  const isFormValid =  isDropdown1Selected && isDropdown2Selected; 
-
-  hiddenButton3.style.display = isFormValid ? 'block' : 'none'; 
-  hiddenNote2.style.display = isFormValid ? 'block' : 'none';
-}
-
-// Event Listeners - Attach to both dropdowns
-dropdown1.addEventListener('change', validateAndShowNext3);
-dropdown2.addEventListener('change', validateAndShowNext3);
 
